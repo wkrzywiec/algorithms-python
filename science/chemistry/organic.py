bacb6d76700e1dee1471437ae8d2494de526cb20
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2020-03-01 16:13:57 +0100

    hydrocarbon class

--- science/chemistry/organic.py
+++ science/chemistry/organic.py
@@ -1,15 +1,6 @@
 import unittest
 import re
 
-def is_alkane(formula):
-    if (__is_hydrocarbon(formula)):
-        carbons = __get_carbons(formula)
-        hydrogens = __get_hydrogens(formula)
-        if 2 * carbons + 2 == hydrogens:
-            return True
-        return False
-    return False
-
 def carbon_content(formula):
     weight = {'H': 1, 'C': 12, 'O': 16}
     carbons = __get_carbons(formula)
@@ -19,6 +10,18 @@ def carbon_content(formula):
         oxygens = __get_oxygens(formula)
     return float(format(100.0 * carbons * weight.get('C') / (carbons * weight.get('C') + hydrogens * weight.get('H') + oxygens * weight.get('O')), '.1f'))
 
+def hydrocarbon_class(formula):
+    if __is_hydrocarbon(formula):
+        hydrogens = __get_hydrogens(formula)
+        carbons = __get_carbons(formula)
+        if __is_alkane(carbons, hydrogens):
+            return "%s is an alkane" % (formula)
+        elif __is_alkene(carbons, hydrogens):
+            return "%s is an alkene" % (formula)
+        elif __is_alkyne(carbons, hydrogens):
+            return "%s is an alkyne" % (formula)
+    return "%s is not a hydrocarbon" % (formula)
+
 def __is_hydrocarbon(formula):
     pattern = 'C\\d*H\\d+'
     if re.match(pattern, formula):
@@ -27,6 +30,21 @@ def __is_hydrocarbon(formula):
         if not (carbons_match is None) and not (hydrogens_match is None):
             return True
         return False
+    return False
+
+def __is_alkane(carbons, hydrogens):
+    if 2 * carbons + 2 == hydrogens:
+        return True
+    return False
+
+def __is_alkene(carbons, hydrogens):
+    if 2 * carbons == hydrogens:
+        return True
+    return False
+
+def __is_alkyne(carbons, hydrogens):
+    if 2 * carbons - 2 == hydrogens:
+        return True
     return False
 
 def __get_carbons(formula):
@@ -40,23 +58,19 @@ def __get_oxygens(formula):
 
 class OrganicTest(unittest.TestCase):
 
-    def test_is_not_alkane(self):
-        self.assertFalse(is_alkane('SFDver0'))
-        self.assertFalse(is_alkane('CH'))
-        self.assertFalse(is_alkane('C2H'))
-        self.assertFalse(is_alkane('C2H2'))
-        self.assertFalse(is_alkane('CH4O'))
-
-    def test_is_alkane(self):
-        self.assertTrue(is_alkane('CH4'))
-        self.assertTrue(is_alkane('C2H6'))
-        self.assertTrue(is_alkane('C5H12'))
-        self.assertTrue(is_alkane('C8H18'))
-        self.assertTrue(is_alkane('C10H22'))
-
     def test_carbon_content(self):
         self.assertEqual(carbon_content('C2H6'), 80.0)
         self.assertEqual(carbon_content('C2H6O'), 52.2)
+
+    def test_hydrocarbon_class(self):
+        self.assertEqual(hydrocarbon_class('DCax'), "DCax is not a hydrocarbon")
+        self.assertEqual(hydrocarbon_class('CH'), "CH is not a hydrocarbon")
+        self.assertEqual(hydrocarbon_class('C2H6'), "C2H6 is an alkane")
+        self.assertEqual(hydrocarbon_class('C6H14'), "C6H14 is an alkane")
+        self.assertEqual(hydrocarbon_class('C2H4'), "C2H4 is an alkene")
+        self.assertEqual(hydrocarbon_class('C5H10'), "C5H10 is an alkene")
+        self.assertEqual(hydrocarbon_class('C2H2'), "C2H2 is an alkyne")
+        self.assertEqual(hydrocarbon_class('C8H14'), "C8H14 is an alkyne")
 
 
 if __name__ == '__main__':
